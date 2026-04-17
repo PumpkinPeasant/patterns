377e3a9ba432b8fa5dc3b9dd79cbc4ffb0778453
Anton <avnikitin@cspfmba.ru>
2026-04-17 13:13:14 +0300

    add inventory styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
+import InventoryBar from "@/components/ui/InventoryBar.vue";
+
+
 </script>
 
 <template>
@@ -10,14 +13,34 @@ import { RouterLink, RouterView } from 'vue-router'
     </nav>
   </header>
 
-  <RouterView />
-
+  <main>
+    <RouterView/>
+  </main>
+  <inventory-bar/>
 
 </template>
 
 <style scoped>
+header {
+  position: sticky;
+  top: 0;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
 nav {
   display: flex;
   gap: 1rem;
+}
+
+main{
+  position: relative;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.inventory-bar{
+  position: fixed;
+  bottom: 0;
 }
 </style>
---
+++ src/components/ui/InventoryBar.vue
@@ -0,0 +1,35 @@
+<script setup>
+const cellsCount = 10;
+</script>
+
+<template>
+  <div class="inventory-bar">
+    <div v-for="n in 10" class="inventory-cell">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.inventory-bar {
+  width: 900px;
+  height: 90px;
+  outline: 10px solid #c6f6d3;
+  left: 50%;
+  bottom: 10px;
+  transform: translateX(-50%);
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  background-color: #a1e1c0;
+}
+
+.inventory-cell{
+  outline: 6px solid #c6f6d3;
+  border: 6px solid #7ecaa4;
+  border-left-color: #59a57f;
+}
+
+.inventory-cell:nth-last-child(1){
+  border-right: none;
+}
+</style>
